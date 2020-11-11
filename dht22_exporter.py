63d46491d7ae989ab8512e9705ead918414d6a4c
gilgamezh <mail@gilgamezh.me>
2020-11-11 20:17:10 +0000

    working version

--- dht22_exporter.py
+++ dht22_exporter.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3
+
 import time
 import logging
 
 import Adafruit_DHT
-from prometheus_client import start_http_server, Summary, Gauge  # fades
+from prometheus_client import start_http_server, Summary, Gauge
 
 
 logger = logging.getLogger("DHT22_exporter")
@@ -21,15 +23,15 @@ humidity_gauge = Gauge("Humidity", "Current Humidity")
 def get_values():
     """Read Temp and Humidity from the DHT22 sensor."""
     logger.debug("Quering sensor. Sensor: %s Pin: %s", SENSOR, PIN)
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    humidity, temperature = Adafruit_DHT.read_retry(SENSOR, PIN)
     logger.info("Got data from the sensor. Temperature: %s, Humidity: %s", temperature, humidity)
-    temperature_gauge.set(temperature_gauge)
+    temperature_gauge.set(temperature)
     humidity_gauge.set(humidity)
     logger.debug("Metrics update done. going to sleep for %s seconds", SLEEP_TIME)
     time.sleep(SLEEP_TIME)
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s  %(levelname)-8s %(message)s")
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s  %(levelname)-8s %(message)s')
     start_http_server(8042)
     # collect in loop
     while True:
